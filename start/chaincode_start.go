ca3967b2eeaaaa21ab4e93fbd794d134b61db8ab
Katsuya SHIMADA <kty.shmd@gmail.com>
2016-12-18 19:40:26 +0900

    add read

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -119,16 +119,33 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
-// Query is our entry point for queries
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("query is running " + function)
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    var key, jsonResp string
+    var err error
 
-	// Handle different functions
-	if function == "dummy_query" {			//read a variable
-		fmt.Println("hi there " + function)	//error
-		return nil, nil;
-	}
-	fmt.Println("query did not find func: " + function)		//error
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+    }
 
-	return nil, errors.New("Received unknown function query: " + function)
+    key = args[0]
+    valAsbytes, err := stub.GetState(key)
+    if err != nil {
+        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+        return nil, errors.New(jsonResp)
+    }
+
+    return valAsbytes, nil
 }
+
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+    fmt.Println("query is running " + function)
+
+    // Handle different functions
+    if function == "read" {                            //read a variable
+        return t.read(stub, args)
+    }
+    fmt.Println("query did not find func: " + function)
+
+    return nil, errors.New("Received unknown function query: " + function)
+}
+
